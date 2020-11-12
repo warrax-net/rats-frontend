b1248934439985273c5e0fc249d526a46680a793
Boris Melikian <boris@lariushin.org>
2020-11-12 16:17:57 +0200

    Custom photo query.

--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <?php
 require_once 'constants.php';
 require_once __DIR__ . '/vendor/autoload.php';
+$url = SERVER_URL . '/api/rat/read.php';
+// Perform GET from URL and authorization headers
+$cURLConnection = curl_init();
+curl_setopt($cURLConnection, CURLOPT_URL, $url);
+curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($cURLConnection);
+$info = null;
+if (!curl_errno($cURLConnection)) {
+    $info = curl_getinfo($cURLConnection, CURLINFO_HTTP_CODE);
+} else {
+    return curl_error($cURLConnection);
+}
+curl_close($cURLConnection);
 
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/view');
-$twig = new \Twig\Environment($loader, [
-    'cache' => __DIR__ . '/compile/cache',
-]);
-$template = $twig->load('index.twig');
+if ($info === 200) {
+    // If response HTTP status code is 200 OK
+    $responseArr = json_decode($response, true); // JSON decode response body
 
-echo $template->render([]);
+    $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/view');
+    $twig = new \Twig\Environment($loader, [
+        'cache' => __DIR__ . '/compile/cache',
+    ]);
+
+    $year = date("Y");
+    $years = [];
+    for ($i = 1998; $i <= $year; $i++) {
+        array_push($years, $i);
+    }
+    $postdata['date_d'] = -1;
+    $postdata['date_m'] = -1;
+    $postdata['date_y'] = -1;
+    $postdata['is_video'] = 0;
+    $post = !empty($_POST) ? $_POST : $postdata;
+    $template = $twig->load('index.twig');
+
+    echo $template->render(['months' => MONTHS, 'days' => DAYS, 'years' => $years, 'post' => $post, 'rats' => $responseArr['body']]);
+}
 ?>
---
+++ photo-form/query.php
@@ -0,0 +1,56 @@
+<?php
+require_once __DIR__ . '/../constants.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$rat_ids = [];
+foreach ($_POST['rat_ids'] as $rat_id) {
+    array_push($rat_ids, $rat_id);
+}
+$json = [
+    'all_alive' => isset($_POST['all_alive']) && $_POST['all_alive'] == '1' ? 1 : 0,
+    'date_m' => $_POST['date_m'],
+    'date_y' => $_POST['date_y'],
+    'rat_ids' => $rat_ids,
+];
+$postdata = json_encode($json);
+// echo $postdata;
+// Set up URL
+$url = SERVER_URL . '/api/photo/query.php';
+// echo $url;
+// Perform GET from URL and authorization headers
+$headers  = [
+    'Content-Type: text/plain'
+];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);           
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_URL, $url);
+
+$response = curl_exec($ch);
+$info = null;
+if (!curl_errno($ch)) {
+    $info = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+} else {
+    return curl_error($ch);
+}
+curl_close($ch);
+
+if ($info === 200) {
+    // If response HTTP status code is 200 OK
+    $responseArr = json_decode($response, true); // JSON decode response body
+    $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../view');
+    $twig = new \Twig\Environment($loader, [
+        'cache' => __DIR__ . '/../compile/cache',
+    ]);
+    $template = $twig->load('photo_index.twig');
+    $year = date("Y");
+    $years = [];
+    for ($i = 1998; $i <= $year; $i++) {
+        array_push($years, $i);
+    }
+    echo $template->render(['months' => MONTHS, 'days' => DAYS, 'years' => $years, 'photos' => $responseArr['body']]);
+} else {
+    echo $url;
+}
